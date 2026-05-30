26c9d2a4ee2dafc6ba6cc9be698346e1acf67df3
YhawB <2425402226@live.gctu.edu.gh>
2026-05-30 06:10:00 +0000

    Add files via upload

---
+++ initials (1) (1) (1).py
@@ -0,0 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Afatsawo Dieu-Donne Mawuli
+# STUDENT ID:  2425402226
+# Email: dieudonnemawuli020@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Afatsawo Dieu-Donne Mawuli, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Print initials A, D, M in stylized large block letters using print and strings only.
+
+    print()
+    print("...ADM")
+    print()
+    print(".......AAAA...........DDDDDDD............MM......MMMM......")
+    print("......AA..AA..........DD.....DD..........MMM....MM.MM......")
+    print(".....AA....AA.........DD......DD.........MMMM..MM..MM......")
+    print(".....AA....AA.........DD......DD.........MM.MM.MM..MM......")
+    print(".....AAAAAAAAA........DD......DD.........MM..MMM...MM......")
+    print(".....AA....AA.........DD......DD.........MM...M....MM......")
+    print(".....AA....AA.........DD......DD.........MM........MM......")
+    print(".....AA....AA.........DD......DD.........MM........MM......")
+    print(".....AA....AA...***...DD.....DD....***...MM........MM...**.")
+    print(".....AA....AA...***...DDDDDDD......***...MM........MM...**.")
+    print()
+
+
+main()
